e248f52f8f7613419521697eec4cac271734a007
Braxton Kappes <braxtonkappes@gmail.com>
2022-04-19 18:54:52 -0500

    fixing reducer

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -2,7 +2,13 @@ import { csrfFetch } from "./csrf.js";
 
 const SET_USER = 'session/setUser';
 const REMOVE_USER = 'session/removeUser';
+const LOAD_ALL_PROFILES = 'profiles/LOAD_ALL_PROFILES';
+const LOAD_ONE_PROFILE = 'profiles/LOAD_ONE_PROFILE';
+const ADD_PROFILE = 'profiles/ADD_PROFILE'
+const REMOVE_PROFILE = 'profiles/REMOVE_PROFILE'
+const EDIT_PROFILE = 'profiles/EDIT_PROFILE'
 
+// actions
 const setUser = (newUser) => ({
   type: SET_USER,
   newUser
@@ -12,6 +18,92 @@ const removeUser = () => ({
   type: REMOVE_USER,
 });
 
+const loadAll = (profiles) => ({
+    type: LOAD_ALL_PROFILES,
+    profiles
+})
+
+const loadOne = (profile) => ({
+    type: LOAD_ONE_PROFILE,
+    profile
+})
+
+const addProfile = (profile) => ({
+    type: ADD_PROFILE,
+    profile
+});
+
+const removeProfile = (profileId) => ({
+    type: REMOVE_PROFILE,
+    profileId
+});
+
+const editProfile = (updatedProfile) => ({
+    type: EDIT_PROFILE,
+    updatedProfile
+});
+
+/* thunks */
+// Get all profiles
+export const loadAllProfiles = (userId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/profiles/all/${userId}`)
+
+    if (res.ok) {
+        const profiles = await res.json();
+        dispatch(loadAll(profiles));
+    }
+}
+
+// Get one profile
+export const loadOneProfile = (profileId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/profiles/one/${profileId}`);
+    if (res.ok) {
+        const profile = await res.json();
+        dispatch(loadOne(profile))
+    }
+}
+
+// Create a profile
+export const createProfile = (data) => async (dispatch) => {
+    const res = await csrfFetch(`/api/profiles`, {
+        method: `POST`,
+        body: JSON.stringify(data)
+    });
+
+    if (res.ok) {
+        const profileData  = await res.json();
+        dispatch(addProfile(profileData.profile));
+        return profileData;
+    }
+}
+
+// Delete a spot
+export const delProfile = (profileId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/profiles/${profileId}`, {
+        method: `DELETE`,
+        body: JSON.stringify({profileId}),
+    });
+    if (res.ok) {
+        const profileId = await res.json();
+        dispatch(removeProfile(profileId))
+        return profileId;
+    }
+}
+
+// Edit a profile
+export const putProfile = (data) => async (dispatch) => {
+    const res = await csrfFetch(`/api/profiles`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+    })
+    if (res.ok) {
+        const updatedProfile = await res.json()
+        dispatch(editProfile(updatedProfile))
+        return updatedProfile;
+    }
+}
+
+// Login user
 export const login = ({ credential, password }) => async dispatch => {
   const response = await csrfFetch("/api/session", {
     method: "POST",
@@ -22,6 +114,7 @@ export const login = ({ credential, password }) => async dispatch => {
   return response;
 };
 
+// Restore user
 export const restoreUser = () => async dispatch => {
   const response = await csrfFetch("/api/session");
   const data = await response.json();
@@ -29,6 +122,7 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+// Signup new user
 export const signup = (user) => async (dispatch) => {
   const { username, email, password } = user;
   const response = await csrfFetch("/api/users", {
@@ -44,6 +138,7 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+// Logout user
 export const logout = () => async (dispatch) => {
   const response = await csrfFetch("/api/session", {
     method: "DELETE",
@@ -53,20 +148,36 @@ export const logout = () => async (dispatch) => {
 };
 
 function reducer(state={
-  user: {
-    
-  },
+  user: { profiles: { channels: { movies: {}}}},
   },action) {
-  let newState={...state};
+  let newState = {...state};
   switch (action.type) {
-    case SET_USER:
-      newState.user = action.newUser;
+    case LOAD_ALL_PROFILES: {
+      action.profiles.forEach(profile => {
+          newState.allProfiles[profile.id] = profile;
+      });
       return newState;
-    case REMOVE_USER:
-      newState = Object.assign({}, state, { user: null });
-      return newState;
-    default:
-      return state;
+    }
+    case ADD_PROFILE: {
+        newState.allProfiles[action.profile.id] = action.profile
+        return newState;
+    }
+    case EDIT_PROFILE: {
+        newState.allProfiles[action.updatedProfile.id] = action.updatedProfile
+        return newState;
+    }
+    case REMOVE_PROFILE: {
+        delete newState.allProfiles[action.profileId]
+        return newState;
+    }
+      case SET_USER:
+        newState.user = action.newUser;
+        return newState;
+      case REMOVE_USER:
+        newState = Object.assign({}, state, { user: null });
+        return newState;
+      default:
+        return state;
   }
 }
 
